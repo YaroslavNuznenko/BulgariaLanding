06ad57407c96aeb0924d0a2a83435975c05115ed
YaroslavNuznenko <nuznenko@gmail.com>
2018-01-23 18:42:07 +0200

    'v1.0.0'

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    nextArrow: '<div class="slider__nextArrow"><i class="fas fa-angle-right arrow-icon "></i></div>',
+    prevArrow: '<div class="slider__prevArrow"><i class="fas fa-angle-left arrow-icon "></i></div>',
+});
